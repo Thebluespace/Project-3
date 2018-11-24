e90fbe8c1392c213b911b52230fd9a3c1d13f0e9
Thomas Greene <tecgreene93@gmail.com>
2018-11-24 12:41:23 -0500

    files

---
+++ project/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+//var db = require("./models");
+const bodyParser = require("body-parser");
+//const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var passport = require('passport');
+var session = require('express-session');
+var env = require('dotenv').load();
+// Add routes, both API and view
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret 
+//app.use(passport.initialize());
+//app.use(passport.session());
+//app.use(routes);
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+app.use(express.static(__dirname + "/public"));
+// Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function() {
+  
+// });
+
+app.listen(PORT, function() {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
