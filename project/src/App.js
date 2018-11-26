24ed9fa9e62445fcf677c6b120cdb818d56e4b1b
Nesmikkuma <Nesmith05@yahoo.com>
2018-11-26 18:14:26 -0500

    stuff

---
+++ project/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header.js';
+import Wrapper from './components/Wrapper/Wrapper.js';
+import Search from "./components/Search/Search.js";
+import Industry from "./components/Industry/Industry.js";
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {/* <Wrapper> */}
+        <div className="columns">
+          <Search />
+        </div>
+        <div className="columns">
+          <Industry />
+        </div>
+        {/* </Wrapper> */}
+      </div>
+
+    );
+  }
+}
+
+export default App;
