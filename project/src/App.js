3b22687db60c785d44118615fd65e73ab53674d9
Nesmikkuma <Nesmith05@yahoo.com>
2018-11-28 21:38:20 -0500

    layout

--- project/src/App.js
+++ project/src/App.js
@@ -6,25 +6,28 @@ import Wrapper from './components/Wrapper/Wrapper.js';
 import Search from "./components/Search/Search.js";
 import Industry from "./components/Industry/Industry.js";
 import Footer from "./components/Footer/Footer.js";
+import Results from "./components/Results/Results.js";
+import Reviews from "./components/Reviews/Reviews.js";
 
 class App extends Component {
 
 
   render() {
     return (
-      <div>
-        <Header />
-        <div className="columns">
-          <Search />
-        </div>
-        <Wrapper>
-          <div className="columns">
-            <Industry />
-          </div>
-        </Wrapper>
+      <Results />
+    //   <div>
+    //     <Header />
+    //     <div className="columns">
+    //       <Search />
+    //     </div>
+    //     <Wrapper>
+    //       <div className="columns">
+    //         <Industry />
+    //       </div>
+    //     </Wrapper>
 
-        <Footer />
-      </div>
+    //     <Footer />
+    //   </div>
       
       
     );
---
+++ project/src/components/Reviews/Reviews.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Reviews.css";
+
+const Reviews = props => (
+    <div>
+        
+        
+        
+    </div>
+);
+
+export default Reviews;
