d5fdce85edc304af9d6eacf90a06a1ca7fb54b2b
R Jones <rajones704@gmail.com>
2018-11-28 19:29:26 -0500

    initialrich

---
+++ project/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header.js';
+import Wrapper from './components/Wrapper/Wrapper.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Header>
+
+
+
+        </Header>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
+
---
+++ project/src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
+const Header = props => (
+ <h1> Who's Bad?????</h1>
+);
+
+export default Header;
