63318f4146bcb0ed99b048fe415f51b18ee4dbad
Nesmikkuma <Nesmith05@yahoo.com>
2018-11-30 10:50:17 -0500

    added new pages

---
+++ notes.md
@@ -0,0 +1,9 @@
+login page
+search page
+results page
+    name, ratings, address, picture, number of reviews - Put it in a card
+comments page 
+    username, user comment, rating?, and a user pic, date and time;
+Hall of Shame: Best of the Worst
+
+https://leaverou.github.io/bubbly/
--- project/src/App.js
+++ project/src/App.js
@@ -6,14 +6,15 @@ import Wrapper from './components/Wrapper/Wrapper.js';
 import Search from "./components/Search/Search.js";
 import Industry from "./components/Industry/Industry.js";
 import Footer from "./components/Footer/Footer.js";
-import Results from "./components/Results/Results.js";
-import Reviews from "./components/Reviews/Reviews.js";
+import Results from "./pages/Results.js";
+import Reviews from "./pages/Reviews.js";
 
 class App extends Component {
 
 
   render() {
     return (
+      // <Reviews />
       // <Results />
       <div>
         <Header />
